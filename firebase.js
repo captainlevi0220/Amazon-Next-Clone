6601a035fc85583c40761d3786211c21bce5f9e3
captainlevi0220 <captainlevi0220@protonmail.com>
2022-03-19 19:58:57 -0400

    setup oauth authentication

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBtjaEjI8bbda5gLZveeHpjgGp1bn_2fSk',
+  authDomain: 'react-next-clone.firebaseapp.com',
+  projectId: 'react-next-clone',
+  storageBucket: 'react-next-clone.appspot.com',
+  messagingSenderId: '94143947108',
+  appId: '1:94143947108:web:0994eb8a7fb606c11a9a71',
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import {
   ShoppingCartIcon,
 } from '@heroicons/react/outline'
 import Image from 'next/image'
-import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Header() {
   return (
@@ -32,7 +32,7 @@ function Header() {
 
         {/* Right  */}
         <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div className=' link'>
+          <div onClick={signIn} className=' link'>
             <p>Hello John Doe</p>
             <p className='font-extrabold md:text-sm'>Account & Lists</p>
           </div>
