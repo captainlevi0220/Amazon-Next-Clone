cf8a36b4bf8900ac4c56837b78537651cf427c07
captainlevi0220 <captainlevi0220@protonmail.com>
2022-03-20 09:05:24 -0400

    css fix for checkout page banner image

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -14,33 +14,36 @@ function Checkout() {
 
       <main className='lg:flex max-w-screen-2xl mx-auto'>
         {/* Left  */}
-        <Image
-          src='https://links.papareact.com/ikj'
-          width={1020}
-          height={250}
-          objectFit='contain'
-        />
+        <div className='flex-grow m-5 shadow-sm'>
+          <Image
+            src='https://links.papareact.com/ikj'
+            width={1020}
+            height={250}
+            objectFit='contain'
+          />
 
-        <div className='flex flex-col p-5 space-y-10 bg-white'>
-          <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
-          {items.length === 0
-            ? 'Your Amazon basket is empty.'
-            : 'Shopping Basket'}
+          <div className='flex flex-col p-5 space-y-10 bg-white'>
+            <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+            {items.length === 0
+              ? 'Your Amazon basket is empty.'
+              : 'Shopping Basket'}
 
-          {items.map((item, i) => (
-            <CheckoutProduct
-              key={i}
-              id={item.id}
-              title={item.title}
-              rating={item.rating}
-              price={item.price}
-              description={item.description}
-              category={item.category}
-              image={item.image}
-              hasPrime={item.hasPrime}
-            />
-          ))}
+            {items.map((item, i) => (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+          </div>
         </div>
+
         {/* Right  */}
         <div></div>
       </main>
