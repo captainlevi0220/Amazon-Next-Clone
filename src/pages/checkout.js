1a4596cd210c3dd2d0b61e24cd182b30bf5180af
captainlevi0220 <captainlevi0220@protonmail.com>
2022-03-20 11:20:12 -0400

    checkout subtotal functionality added

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from '../components/Header'
 import Image from 'next/image'
 import { useSelector } from 'react-redux'
-import { selectItems } from '../slices/basketSlice'
+import { selectItems, selectTotal } from '../slices/basketSlice'
 import CheckoutProduct from '../components/CheckoutProduct'
 import Currency from 'react-currency-formatter'
+import { useSession } from 'next-auth/react'
 
 function Checkout() {
   const items = useSelector(selectItems)
+  const total = useSelector(selectTotal)
+  const { data: session } = useSession()
   return (
     <div className='bg-gray-100'>
       <Header />
@@ -45,7 +48,28 @@ function Checkout() {
         </div>
 
         {/* Right  */}
-        <div></div>
+        <div className='flex flex-col bg-white p-10 shadow-md'>
+          {items.length > 0 && (
+            <>
+              <h2>
+                Subtotal ({items.length} items):{' '}
+                <span className='font-bold'>
+                  <Currency quantity={total} currency='USD' />
+                </span>
+              </h2>
+
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session &&
+                  'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+                }`}
+              >
+                {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   )
